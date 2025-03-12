317da6276de72d279b502015933ff34696532fdd
vansh845 <koulvansh845@gmail.com>
2025-03-12 17:13:10 +0530

    changed setting

--- book/models.py
+++ book/models.py
@@ -18,3 +18,4 @@ class Book(models.Model):
 
     def __str__(self):
         return self.name
+
--- books_api/settings.py
+++ books_api/settings.py
@@ -32,7 +32,8 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'rest_framework',
-    'book.apps.BookConfig',
+    'book',
+
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
